ed3b9d155741240fb6d3096b8c2083b450d1a2c2
yurisokolovicz <yurisokolovicz@gmail.com>
2023-03-19 11:26:28 -0300

    Styled Components & Media Queries

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+// import './CourseInput.css';
 
 const FormControl = styled.div`
     margin: 0.5rem 0;
